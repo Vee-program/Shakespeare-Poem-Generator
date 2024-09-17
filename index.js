cf1502a8035d28e68bcfc86734b86abd4091499c
Vee-program <masilelathembelihle44@gmail.com>
2024-09-17 14:36:18 +0200

    updated the html to improve user experience by adding an example of a topic

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let poemTopic = document.querySelector("#poem-topic");
   let apiKey = "19fa4a86457804tcabc33b0a088f366o";
   let context =
-    "You are an expert at romantic and love poems. Please generate a short love poem of 5 lines, please use the <br/> so that each sentence can start on a new line, please also include a title of the poem on top and use CSS fontweight: bold so that it can be bold, can you please use <br/> again after the title.";
+    "You are an expert at love and romantic poems,Please generate a short love poem of 5 lines according to either the topic, please use the <br/> so that each sentence can start on a new line.";
   let prompt = `Please generate a short love poem according to ${poemTopic}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showPoem);
